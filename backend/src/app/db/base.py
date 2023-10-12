28fa87e715e4853ba77ac74c70f04dc2c7f6c5f2
mzdevI <muhammed.mammadhuseynov@gmail.com>
2023-10-12 17:10:02 +0400

    add alembic

--- backend/src/app/db/base.py
+++ backend/src/app/db/base.py
@@ -1,8 +1,12 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 
+# from sqlalchemy import MetaData
+
 from base_class import Base
 from ..models.user import User
 from ..models.booking import Booking
 from ..models.form import Form
 from ..models.question import Question
+
+# metadata = MetaData()
